614a7cadd9c02777061094854c0cb1215ec599d4
Paulo Souza <paulosouzx14@gmail.com>
2024-08-28 12:58:40 +0100

    Criação da ação de Deletar Sellers

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -45,5 +45,29 @@ namespace SalesWebMvc.Controllers
             var viewModel = new SellerFormViewModels { Seller = seller, Departments = departments };
             return View(viewModel);
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if ( id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
