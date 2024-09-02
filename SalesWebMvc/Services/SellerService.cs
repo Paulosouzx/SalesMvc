83b803d7eeb6ff4cb4cf4b34628672a3fc5f4187
Paulo Souza <paulosouzx14@gmail.com>
2024-09-02 16:38:38 +0100

    Tratamento da exceção para delete

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -32,16 +32,23 @@ namespace SalesWebMvc.Services
             return await _context.Seller.Include(obj => obj.Department).FirstOrDefaultAsync(x => x.Id == id);
         }
 
-        public async Task Remove(int id)
+        public async Task RemoveAsync(int id)
         {
+            try
+            {
             var obj = await _context.Seller.FindAsync(id);
             _context.Seller.Remove(obj);
             await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateException e)
+            {
+                throw new IntegrityException("Can't delete seller because he/she has sales");
+            }
         }
 
         public async Task UpdateAsync(Seller obj)
         {
-            bool hasAny = await _context.Seller.Any(x => x.Id == obj.Id);
+            bool hasAny = await _context.Seller.AnyAsync(x => x.Id == obj.Id);
             if (!hasAny) 
             {
                 throw new NotFoundException("Id not Found");
